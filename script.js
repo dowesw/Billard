dc03168a364662fc9924ed17fe7b6397ee66d59a
Dowes Mbella <dowesw@gmail.com>
2022-02-16 15:29:46 +0100

    Creation des fonctions de calcul de vitesse

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let x = 50; //position horizontal de la balle
 let y = 500; //position verticale de la balle
 let dt = 10; //dure de deplacement de la balle
 let p = 2; //puissance de la frappe de la balle
-let d = 1; // decelleration de la vitesse de la basse
-let c = 0.78; // coefficient de rebus
+let d = 1.1; // decelleration de la vitesse de la basse
+let c = 1; // coefficient de rebus
 
-let angle = 1;
+let angle = 360;
 let vx = 10; //vitesse deplacement horizontal de la balle
 let vy = 0; //vitesse deplacement vertical de la balle
 
@@ -24,19 +24,19 @@ async function onPlay() {
     while (!stop) {
         let obstacle = !(x < (width - (r * 2)));
         while (!obstacle) {
-            vx = vx - (vx * (d / 100))
+            vx = getVxAvant();
             x += vx;
+            y += 1;
             balle.style.left = x + "px";
             balle.style.top = y + "px";
             stop = vx <= 0;
             obstacle = !(x < (width - (r * 2))) || x <= 0;
             await sleep(dt);
         }
-        console.log(0);
         stop = vx <= 0;
         obstacle = x <= 0;
         while (!obstacle) {
-            vx = vx - (vx * (c / 100));
+            vx = getVxApres();
             x -= vx;
             balle.style.left = x + "px";
             balle.style.top = y + "px";
@@ -44,12 +44,34 @@ async function onPlay() {
             obstacle = !(x < (width - (r * 2))) || x <= 0;
             await sleep(dt);
         }
-        console.log('stop', stop);
     }
     balle.style.top = "50px";
-    balle.style.left = "50px";
+    balle.style.left = "500px";
 }
 
 async function sleep(msec) {
     return new Promise(resolve => setTimeout(resolve, msec));
+}
+
+function getVxAvant() {
+    return vx - (vx * (d / 100));
+}
+
+function getVxApres() {
+    return vx - (vx * ((d * c) / 100));
+}
+
+async function getAngle(axe) {
+    if (angle >= 380) {
+        return 0;
+    }
+    switch (axe) {
+        case 'vertical':
+
+            break;
+        case 'horizontal':
+
+            break;
+    }
+    return 0;
 }
