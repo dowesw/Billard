9a74c4f4a2bdc3790536c248d8ebe91ffe52252e
Dowes Mbella <dowesw@gmail.com>
2022-02-17 20:09:27 +0100

    correction du code pour la bonne définition des valeurs des variables

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const width = screen.width;
+const height = screen.height;
 
 let r = 25; //rayon de la balle
 let x = 50; //position horizontal de la balle
 let y = 500; //position verticale de la balle
-let dt = 10; //dure de deplacement de la balle
-let p = 2; //puissance de la frappe de la balle
-let d = 1.1; // decelleration de la vitesse de la basse
-let c = 1; // coefficient de rebus
+let dt = 1000; //dure de deplacement de la balle en ms
+let p = 1; //puissance de la frappe de la balle
+let d = 0.5; // decelleration de la vitesse de la basse
+let c = 0.78; // coefficient de rebus
 
 let angle = 360;
 let vx = 10; //vitesse deplacement horizontal de la balle
@@ -16,36 +17,44 @@ let balle = document.getElementById("balle");
 balle.style.top = y + "px";
 balle.style.left = x + "px";
 
-async function onPlay() {
-    let stop = false;
-    vx = 10;
-    vy = 0;
-    vx = vx * p;
-    while (!stop) {
-        let obstacle = !(x < (width - (r * 2)));
-        while (!obstacle) {
-            vx = getVxAvant();
-            x += vx;
-            balle.style.left = x + "px";
-            balle.style.top = y + "px";
-            stop = vx <= 0;
-            obstacle = !(x < (width - (r * 2))) || x <= 0;
-            await sleep(dt);
-        }
-        stop = vx <= 0;
-        obstacle = x <= 0;
-        while (!obstacle) {
-            vx = getVxApres();
-            x -= vx;
-            balle.style.left = x + "px";
-            balle.style.top = y + "px";
-            stop = vx <= 0;
-            obstacle = !(x < (width - (r * 2))) || x <= 0;
-            await sleep(dt);
-        }
+async function onClick() {
+    vx *= p;
+    vy *= p;
+    update();
+}
+
+async function update() {
+    vx -= (vx * d / 100);
+    vy -= (vy * d / 100);
+    x += vx;
+    y += vy;
+    setPosition();
+    if (x < (r * 2)) {
+        x = r * 2;
+        vx *= -c;
     }
-    balle.style.top = "50px";
-    balle.style.left = "500px";
+    if (x > width - (r * 2)) {
+        x = width - (r * 2);
+        vx *= -c;
+    }
+    if (y < (r * 2)) {
+        y = r * 2;
+        vy *= -c;
+    }
+    if (y > height - (r * 2)) {
+        y = height - (r * 2);
+        vy *= -c;
+    }
+}
+
+async function setPosition() {
+    if ((vx > 0 && vx < 0.001) || (vx < 0 && vx > 0.001) || (vy > 0 && vy < 0.001) || (vy < 0 && vy > 0.001)) {
+        return;
+    }
+    balle.style.left = x + "px";
+    balle.style.top = y + "px";
+    await sleep(dt / 100);
+    update();
 }
 
 async function sleep(msec) {
@@ -58,19 +67,4 @@ function getVxAvant() {
 
 function getVxApres() {
     return vx - (vx * ((d * c) / 100));
-}
-
-async function getAngle(axe) {
-    if (angle >= 380) {
-        return 0;
-    }
-    switch (axe) {
-        case 'vertical':
-
-            break;
-        case 'horizontal':
-
-            break;
-    }
-    return 0;
 }
